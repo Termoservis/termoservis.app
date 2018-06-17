91c93c5d79202c5f2893598921fdf4059054e63a
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2018-06-17 20:48:41 +0200

    Improved hover table visuals

--- src/components/Table/TableCell.jsx
+++ src/components/Table/TableCell.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { resolveColumnClassName } from './TableHelpers';
 
 const TableCell = ({ to, children, ...rest }) => (
-    <td className={resolveColumnClassName(rest)}>
+    <td className={resolveColumnClassName({ to, ...rest })}>
         {to
             ? (
                 <Link to={to}>{children}</Link>
--- src/components/Table/TableHelpers.js
+++ src/components/Table/TableHelpers.js
@@ -1,6 +1,7 @@
 import classnames from 'classnames';
 
 export const resolveColumnClassName = props => classnames(
+    props && props.to && 'link',
     props && props.number && 'number',
     props && props.actions && 'actions',
     props && props.className
